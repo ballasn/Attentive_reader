94f7398d89dd2c37199b4eef731f5058a29926ba
Tim Cooijmans <cooijmans.tim@gmail.com>
2016-03-28 23:50:27 -0400

    add scripts used for training

---
+++ train_bn-everywhere.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+DIR=~/dev/Attentive_reader/codes/
+SDIR=$DIR/att_reader/
+MDIR=/data/lisatmp3/cooijmat/run/batchnorm/attr/batchnorm-everywhere
+export PYTHONPATH=$DIR:$PYTHONPATH
+
+tg 2 ${SDIR}/train_attentive_reader.py \
+    --use_dq_sims 1 --use_desc_skip_c_g 0 --dim 240 --learn_h0 1 --lr 8e-5 --truncate -1 \
+    --model "default.npz" --batch_size 64 --unit_type bnlstm \
+    --optimizer "adam" --validFreq 1000 --model_dir $MDIR --use_desc_skip_c_g 1 \
+    --bn-everywhere
