41c98d86bbc7efb4dc1fb638737f34f49dad991e
Tim Cooijmans <cooijmans.tim@gmail.com>
2016-04-07 17:45:51 -0400

    prepare full experiments

--- train_batchnorm.sh
+++ train_batchnorm.sh
@@ -5,7 +5,7 @@ SDIR=$DIR/att_reader/
 MDIR=/data/lisatmp3/cooijmat/run/batchnorm/attr/
 export PYTHONPATH=$DIR:$PYTHONPATH
 
-tg 4 ${SDIR}/train_attentive_reader.py \
+tg 2 ${SDIR}/train_attentive_reader.py \
     --use_dq_sims 1 --use_desc_skip_c_g 0 --dim 280 --learn_h0 1 --lr 8e-5 --truncate -1 \
-    --model "baseline.npz" --batch_size 64 \
+    --model "batchnorm.npz" --batch_size 64 --bn-everywhere \
     --optimizer "adam" --validFreq 1000 --model_dir $MDIR --use_bidir 1
